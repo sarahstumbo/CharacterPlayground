d6be51b9c948b8a25977a7e92780f13e62398c02
sarah <sh@jadu.ar>
2022-11-11 11:46:30 -0500

    add project files

---
+++ Assets/KinematicCharacterController/ExampleCharacter/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KinematicCharacterController.Examples
+{
+    public class Player : MonoBehaviour
+    {
+        public CharacterController Character;
+        
+        private const string HorizontalInput = "Horizontal";
+        private const string VerticalInput = "Vertical";
+
+        private void Update()
+        {
+            HandleCharacterInput();
+        }
+
+        private void HandleCharacterInput()
+        {
+            PlayerCharacterInputs characterInputs = new PlayerCharacterInputs();
+
+            // Build the CharacterInputs struct
+            characterInputs.MoveAxisForward = Input.GetAxisRaw(VerticalInput);
+            characterInputs.MoveAxisRight = Input.GetAxisRaw(HorizontalInput);
+            characterInputs.JumpDown = Input.GetKeyDown(KeyCode.Space);
+            characterInputs.CrouchDown = Input.GetKeyDown(KeyCode.C);
+            characterInputs.CrouchUp = Input.GetKeyUp(KeyCode.C);
+
+            // Apply inputs to character
+            Character.SetInputs(ref characterInputs);
+        }
+    }
+}
